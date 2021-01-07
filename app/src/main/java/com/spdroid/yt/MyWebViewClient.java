7f7f5c1f9614253421ce2b50d146c2069f22ce2c
iiamvk <iiamvinod@gmail.com>
2021-01-07 18:40:35 +0530

    Initial commit

---
+++ app/src/main/java/com/spdroid/yt/MyWebViewClient.java
@@ -0,0 +1,14 @@
+package com.spdroid.yt;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by Admin on 20-Dec-16.
+ */
+public class MyWebViewClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        return false;
+    }
+}
